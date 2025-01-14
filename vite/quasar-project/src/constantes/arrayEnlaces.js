65801a5dc06080812a41fd5830ad01b6683508d3
GuidoFC <guidofigueroa96@gmail.com>
2025-01-14 23:54:31 +0100

    he creado un icono para voler al panel principal

--- vite/quasar-project/src/constantes/arrayEnlaces.js
+++ vite/quasar-project/src/constantes/arrayEnlaces.js
@@ -1,6 +1,12 @@
 // pongo todas los enlaces en una unica clase para luego exportarla y usarla donde yo quiera
 export const linksListArray = [
   {
+    title: 'Home',
+    caption: '', // short description
+    icon: 'home', // icon can be select from this page -> https://fonts.google.com/icons?selected=Material+Symbols+Outlined:group:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=user&icon.size=24&icon.color=%23e8eaed
+    link: '/' // Actualizado para usar la ruta interna
+  },
+  {
     title: 'Gestionar Usuario',
     caption: 'CRUD usuario', // short description
     icon: 'group', // icon can be select from this page -> https://fonts.google.com/icons?selected=Material+Symbols+Outlined:group:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=user&icon.size=24&icon.color=%23e8eaed
