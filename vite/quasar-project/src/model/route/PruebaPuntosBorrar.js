d234960617b7726bff89409766608cf4a19181fc
GuidoFC <guidofigueroa96@gmail.com>
2025-01-29 17:53:49 +0100

    añadiendo archivos para hacer la prueba de poder visualizar los puntos en google maps

---
+++ vite/quasar-project/src/model/route/PruebaPuntosBorrar.js
@@ -0,0 +1,99 @@
+
+export const puntos  = [
+  {
+    latitude: 39.6757345553,
+    longitude: 2.9729708657,
+    elevation: 67.62,
+    time: "2017-10-14T06:53:20"
+  },
+  {
+    latitude: 39.6756667458,
+    longitude: 2.9729743861,
+    elevation: 67.28,
+    time: "2017-10-14T06:53:21"
+  },
+  {
+    latitude: 39.6756048873,
+    longitude: 2.9729492404,
+    elevation: 66.80,
+    time: "2017-10-14T06:53:22"
+  },
+  {
+    latitude: 39.6755574457,
+    longitude: 2.9729059059,
+    elevation: 66.06,
+    time: "2017-10-14T06:53:23"
+  },
+  {
+    latitude: 39.6755169611,
+    longitude: 2.9728584643,
+    elevation: 65.32,
+    time: "2017-10-14T06:53:24"
+  },
+  {
+    latitude: 39.6754347347,
+    longitude: 2.9727611504,
+    elevation: 64.29,
+    time: "2017-10-14T06:53:26"
+  },
+  {
+    latitude: 39.6749875601,
+    longitude: 2.9723782651,
+    elevation: 64.33,
+    time: "2017-10-14T06:53:35"
+  },
+  {
+    latitude: 39.6743483562,
+    longitude: 2.9718677234,
+    elevation: 65.17,
+    time: "2017-10-14T06:53:48"
+  },
+  {
+    latitude: 39.6735382453,
+    longitude: 2.9712130129,
+    elevation: 67.47,
+    time: "2017-10-14T06:54:05"
+  },
+  {
+    latitude: 39.6729518473,
+    longitude: 2.9707385134,
+    elevation: 69.05,
+    time: "2017-10-14T06:54:18"
+  },
+  {
+    latitude: 39.6723692212,
+    longitude: 2.9702589847,
+    elevation: 71.19,
+    time: "2017-10-14T06:54:31"
+  },
+  {
+    latitude: 39.6718999185,
+    longitude: 2.9698283225,
+    elevation: 71.96,
+    time: "2017-10-14T06:54:41"
+  },
+  {
+    latitude: 39.6715109982,
+    longitude: 2.9694384802,
+    elevation: 73.31,
+    time: "2017-10-14T06:54:50"
+  },
+  {
+    latitude: 39.6709711198,
+    longitude: 2.9689334705,
+    elevation: 74.90,
+    time: "2017-10-14T06:55:02"
+  },
+  {
+    latitude: 39.6708460618,
+    longitude: 2.9688391741,
+    elevation: 75.10,
+    time: "2017-10-14T06:55:05"
+  },
+  {
+    latitude: 39.6704523638,
+    longitude: 2.9686105996,
+    elevation: 76.44,
+    time: "2017-10-14T06:55:14"
+  }
+];
