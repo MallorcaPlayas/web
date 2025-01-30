293af6d3de6e83aadbfb064723c78a1667df05c4
GuidoFC <guidofigueroa96@gmail.com>
2025-01-30 20:06:46 +0100

    Mapeo para RouteService.js

--- vite/quasar-project/src/service/RouteService.js
+++ vite/quasar-project/src/service/RouteService.js
@@ -11,13 +11,48 @@ export class RouteService {
     })
     const routes = await data.json()
 
-    return routes.map(route => {
-      return new Route(route.id, route.name, route.distance, route.duration, route.elevation,
-        route.distance,
-        new User(route.user.id, route.user.name, route.user.userName, route.user.firstName, route.user.secondUsername, route.user.email, route.user.birthday, route.user.urlPhoto, route.user.privatePrivacy, route.user.state, null),
-        route.private
-      )
-    })
+    console.log("Que recibo de SPingboot?", routes)
+
+    return routes
+      .filter(route => {
+        // Filtrar las rutas donde route.user no existe o está mal definido
+        if (!route.user) {
+          console.log(`El usuario de la ruta con ID ${route.id} es null o undefined. Ruta excluida.`);
+          return false; // Excluir esta ruta
+        }
+
+        if (!route.user.id) {
+          console.log(`El usuario de la ruta con ID ${route.id} no tiene un ID asignado. Ruta excluida.`);
+          return false; // Excluir esta ruta
+        }
+
+        return true; // Incluir esta ruta
+      })
+      .map(route => {
+        // Aquí estamos seguros de que route.user existe y tiene un ID
+        return new Route(
+          route.id,
+          route.name,
+          route.distance,
+          route.duration,
+          route.elevation,
+          route.locations, // TODO: Faltara mapear este Array
+          new User(
+            route.user.id,
+            route.user.name,
+            route.user.userName,
+            route.user.firstSurname,
+            route.user.secondSurname,
+            route.user.email,
+            route.user.birthday,
+            route.user.urlPhoto,
+            route.user.privatePrivacy,
+            route.user.state,
+            null
+          ),
+          route.private
+        );
+      });
   }
 
   create(route) {
