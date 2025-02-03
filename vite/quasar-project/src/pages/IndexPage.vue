2bf4be895d7e202ab4d0dc6201fa2dfc3e0ee777
Danylo <dshvalia612@alumnes.politecnicllevant.cat>
2025-02-03 16:48:11 +0100

    Eliminando .idea y .quasar del repo

--- vite/quasar-project/src/pages/IndexPage.vue
+++ vite/quasar-project/src/pages/IndexPage.vue
@@ -1,30 +1,21 @@
 <template>
   <!--  Añadir un titulo de panel de administracion-->
-
   <q-item-label
     header
   >
     Panel de Administración
   </q-item-label>
-
-
   <q-page class="flex justify-around">
-
     <EssentialLink
       v-for="link in linksList"
       :key="link.title"
       v-bind="link"
     />
-
-
   </q-page>
 
 </template>
 
 <script setup>
-//
-
-
 import {ref} from 'vue'
 import EssentialLink from 'components/EssentialLink.vue'
 
