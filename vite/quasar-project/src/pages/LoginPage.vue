48d4b39d6a4bf3f34aca59954a04fbce80ff66d8
GuidoFC <guidofigueroa96@gmail.com>
2025-02-05 13:23:56 +0100

    Ya puedo hacer el login con google, puedo recibir el token y enviarlo a spring para que este me genere un token de authentificacion

--- vite/quasar-project/src/pages/LoginPage.vue
+++ vite/quasar-project/src/pages/LoginPage.vue
@@ -3,14 +3,45 @@ import {
   GoogleSignInButton,
 } from "vue3-google-signin";
 
+import {UserAuthenticationService} from "../service/UserAuthenticationService.js";
 import GoogleSignInPlugin from "vue3-google-signin"
 
 
-
 // handle success event
-const handleLoginSuccess = (response) => {
-  const { credential } = response;
+const handleLoginSuccess = async  (response) => {
+  const {credential} = response;
   console.log("Access Token", credential);
+
+  // Use the access token to authenticate with your server
+
+  const UserAuthentication = new UserAuthenticationService();
+
+  const getTokenSpring = await UserAuthentication.getTokenSpring(credential);
+
+  console.log("Token Spring", getTokenSpring);
+
+
+
+
+  // Guardar el token en localStorage
+  if (getTokenSpring) {
+    localStorage.setItem("authToken", getTokenSpring);
+    console.log("Token Spring guardado en localStorage:", getTokenSpring);
+  } else {
+    console.error("No se recibió un token válido desde el servidor.");
+  }
+
+  const token = localStorage.getItem("authToken");
+
+
+
+  if (token) {
+    console.log("Token recuperado del LocalStorage:", token);
+    // Aquí puedes usar el token donde lo necesites
+  } else {
+    console.error("No se encontró el token en localStorage");
+  }
+
 };
 
 // handle an error event
---
+++ vite/quasar-project/src/service/UserAuthenticationService.js
@@ -0,0 +1,27 @@
+
+export class UserAuthenticationService {
+  #URL = "http://localhost:8080/api/auth/loginGoogleAuth"
+
+  async getTokenSpring(tokenGoogleAuth) {
+    try {
+      const response = await fetch(this.#URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams({ tokenGoogle: tokenGoogleAuth }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.statusText}`);
+      }
+
+      // Devuelve el token del cuerpo de la respuesta
+      const token = await response.text();
+      console.log(token);
+      return token;
+    } catch (error) {
+      console.error("Error al obtener el token:", error);
+      throw error;
+    }
+  }
+}
+
