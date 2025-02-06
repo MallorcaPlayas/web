12e39406d4d3bdec702283670ea6f77289d0dbd3
GuidoFC <guidofigueroa96@gmail.com>
2025-02-06 18:57:37 +0100

    Envio el token en cada peticion para recibir los datos de de la api

--- vite/quasar-project/src/service/BeachService.js
+++ vite/quasar-project/src/service/BeachService.js
@@ -8,39 +8,65 @@ import {ServiceBeach} from "src/model/beach/ServiceBeach.js";
 export class BeachService {
   #URL = "http://localhost:8080/api/beaches"
 
+  #tokenSpring = localStorage.getItem('authToken');
+
   async getAll() {
-    const data = await fetch(this.#URL, {
-      method: "GET",
-      headers: {'Content-Type': 'application/json'},
-    });
+    try {
 
-    const beaches = await data.json()
+      if (!this.#tokenSpring) {
+        throw new Error("El token de autorización no está disponible.");
+      }
 
-    return beaches.map(beach => {
-      return new Beach(beach.id, beach.name, beach.description,
-        beach.types.map(type => {
-          return new TypeBeach(type.id, type.name);
-        }),
-        beach.cameras.map(camera => {
-          return new Camera(camera.id, camera.url);
-        }),
-        beach.usersInCharge.map((userInCharge) => {
-          return new User(userInCharge.user.id, userInCharge.user.name, userInCharge.user.userName, userInCharge.user.firstSurname, userInCharge.user.secondSurname, userInCharge.user.email, userInCharge.user.birthday, userInCharge.user.urlPhoto, userInCharge.user.privatePrivacy, userInCharge.user.state, null
-          );
-        }),
-        beach.services.map(service => {
-          return new Service(service.id,
-            new ServiceBeach(service.serviceBeach.id, service.serviceBeach.name),
-            service.startTime, service.endTime);
-        })
-      )
-    })
+      const data = await fetch(this.#URL, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + this.#tokenSpring
+        },
+      });
+
+      if (!data.ok) {
+        throw new Error(`HTTP Error en el servidor : ${data.status}`);
+      }
+
+      const beaches = await data.json()
+
+      return beaches.map(beach => {
+        return new Beach(beach.id, beach.name, beach.description,
+          beach.types.map(type => {
+            return new TypeBeach(type.id, type.name);
+          }),
+          beach.cameras.map(camera => {
+            return new Camera(camera.id, camera.url);
+          }),
+          beach.usersInCharge.map((userInCharge) => {
+            return new User(userInCharge.user.id, userInCharge.user.name, userInCharge.user.userName, userInCharge.user.firstSurname, userInCharge.user.secondSurname, userInCharge.user.email, userInCharge.user.birthday, userInCharge.user.urlPhoto, userInCharge.user.privatePrivacy, userInCharge.user.state, null
+            );
+          }),
+          beach.services.map(service => {
+            return new Service(service.id,
+              new ServiceBeach(service.serviceBeach.id, service.serviceBeach.name),
+              service.startTime, service.endTime);
+          })
+        )
+      })
+
+    } catch (error) {
+      console.error("Error al obtener las playas:", error);
+      return null;
+      throw error
+    }
+
+
   }
 
   create(beach) {
     fetch(this.#URL, {
       method: "POST",
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.#tokenSpring
+      },
       body: JSON.stringify(beach)
     });
   }
@@ -48,13 +74,19 @@ export class BeachService {
   update(beach) {
     fetch(this.#URL + "/" + 1, {
       method: "PUT",
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.#tokenSpring
+      },
       body: JSON.stringify(beach)
     });
   }
 
   delete(id) {
     fetch(this.#URL + "/" + id, {
+      headers: {
+        'Authorization': 'Bearer ' + this.#tokenSpring
+      },
       method: "DELETE",
     });
   }
