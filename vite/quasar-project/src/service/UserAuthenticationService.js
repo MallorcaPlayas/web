aea82b879bcd082fb1d91be51d19116a2f75ca9d
GuidoFC <guidofigueroa96@gmail.com>
2025-02-06 08:36:36 +0100

    logica para poder enviar el token, username o email para poder hacer la authentificacion por spring

--- vite/quasar-project/src/service/UserAuthenticationService.js
+++ vite/quasar-project/src/service/UserAuthenticationService.js
@@ -1,13 +1,38 @@
-
 export class UserAuthenticationService {
-  #URL = "http://localhost:8080/api/auth/loginGoogleAuth"
+  #URLGoogle = "http://localhost:8080/api/auth/loginGoogleAuth"
+  #URL = "http://localhost:8080/api/auth/login"
 
-  async getTokenSpring(tokenGoogleAuth) {
+  async getTokenSpringGoogleAuth(tokenGoogleAuth) {
+    try {
+      const response = await fetch(this.#URLGoogle, {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: new URLSearchParams({tokenGoogle: tokenGoogleAuth}),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error en la solicitud: ${response.statusText}`);
+      }
+
+      // Devuelve el token del cuerpo de la respuesta
+      const token = await response.text();
+      return token;
+    } catch (error) {
+      console.error("Error al obtener el token:", error);
+      throw error;
+    }
+  }
+
+
+  async getTokenSpringUserNameOrEmail(usernameorEmail, password) {
     try {
       const response = await fetch(this.#URL, {
         method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams({ tokenGoogle: tokenGoogleAuth }),
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: new URLSearchParams({
+          username: usernameorEmail,
+          password: password
+        }),
       });
 
       if (!response.ok) {
