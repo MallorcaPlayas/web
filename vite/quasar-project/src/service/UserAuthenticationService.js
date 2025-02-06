3c3b92aca92a5e6d343ad4e4c8d43bcfcf0a9ffe
GuidoFC <guidofigueroa96@gmail.com>
2025-02-06 09:31:43 +0100

    Si tengo un error del servidor es porque el usuario no ha introducido correctamente el usuario o contraseña, por lo tanto, me devuelve un null

--- vite/quasar-project/src/service/UserAuthenticationService.js
+++ vite/quasar-project/src/service/UserAuthenticationService.js
@@ -19,6 +19,7 @@ export class UserAuthenticationService {
       return token;
     } catch (error) {
       console.error("Error al obtener el token:", error);
+      return null;
       throw error;
     }
   }
@@ -44,6 +45,7 @@ export class UserAuthenticationService {
       return token;
     } catch (error) {
       console.error("Error al obtener el token:", error);
+      return null;
       throw error;
     }
   }
