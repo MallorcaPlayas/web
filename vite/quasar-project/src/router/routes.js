ee37a933619d7c6a6718373b053ce688ca5cf733
Danylo <dshvalia612@alumnes.politecnicllevant.cat>
2025-02-08 00:42:11 +0100

    small fix

--- vite/quasar-project/src/router/routes.js
+++ vite/quasar-project/src/router/routes.js
@@ -15,53 +15,53 @@ const routes = [
       {
         path: '',
         component: () => import('pages/IndexPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: 'routes',
         component: () => import('pages/RoutePage.vue'),
-        // meta: {requiresAuth: false},
+        meta: {requiresAuth: false},
       },
       {
         path: 'login',
         component: () => import('pages/LoginPage.vue'),
-        // meta: {isAuth: true},
+        meta: {isAuth: true},
       },
       {
         path: 'user',
         component: () => import('pages/UserPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
 
         path: 'beaches',
         component: () => import('pages/BeachPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: 'excursions',
         component: () => import('pages/ExcursionPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: 'roles',
         component: () => import('pages/RolePage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: 'role-requests',
         component: () => import('pages/RoleRequestPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: 'complaints',
         component: () => import('pages/ComplaintsPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: 'points-of-interest',
         component: () => import('pages/PointsOfInterestPage.vue'),
-        // meta: {requiresAuth: true},
+        meta: {requiresAuth: true},
       },
       {
         path: ':catchAll(.*)*',
@@ -70,24 +70,5 @@ const routes = [
     ]
   },
 ]
-
-// export default routes
-//       {path: '', component: () => import('pages/IndexPage.vue'), meta: {requiresAuth: true}} // Protegido, primero haces login
-//     ]
-//   },
-//   {path: '/login', component: () => import('pages/LoginPage.vue'), meta: {isAuth: true}},
-//   {path: '/user', component: () => import('layouts/manageUser.vue'), meta: {requiresAuth: true}},
-//   {path: '/beaches', component: () => import('layouts/managerBeach.vue'), meta: {requiresAuth: true}},
-//   {path: '/excursions', component: () => import('layouts/managerExcursions.vue'), meta: {requiresAuth: true}},
-//   {path: '/roles', component: () => import('layouts/managerRole.vue'), meta: {requiresAuth: true}},
-//   {path: '/role-requests', component: () => import('layouts/managerRoleRequest.vue'), meta: {requiresAuth: true}},
-//   {path: '/complaints', component: () => import('layouts/managerComplaints.vue'), meta: {requiresAuth: true}},
-//   {
-//     path: '/points-of-interest',
-//     component: () => import('layouts/managerPointsOfInterest.vue'),
-//     meta: {requiresAuth: true}
-//   },
-//   {path: '/:catchAll(.*)*', component: () => import('pages/ErrorNotFound.vue')}
-// ];
 
 export default routes;
