9c6a470234d517dd32c60e75d947532f03d15438
GuidoFC <guidofigueroa96@gmail.com>
2025-02-14 13:53:13 +0100

    quitando el hola

--- vite/quasar-project/src/service/OrganizationService.js
+++ vite/quasar-project/src/service/OrganizationService.js
@@ -6,7 +6,6 @@ export class OrganizationService {
   #BASE_PATH = `organizations`;
 
   async getAll() {
-    console.log("HOLA!!!")
     const data = (await api.get(this.#BASE_PATH)).data;
     return data.map(organization => Organization.fromJson(organization));
   }
